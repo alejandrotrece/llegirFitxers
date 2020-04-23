a94b923cb2f2e81ef0e9305987c4a839587b3a67
alejandrotrece <alejandro.martinez@iesjoandaustria.org>
2020-04-23 09:46:09 +0000

    Exercici 2 fet

--- podium.py
+++ podium.py
@@ -4,10 +4,13 @@ import os
 fitxers = os.listdir('curses/')
 #per cada arxiu que està al directori curses
 for nomFitxer in fitxers:
-    #nomFitxer conté el nom de l'arxiu que està en el directori curses 
-    #TODO heu de posar a la variable fitxer l'obertura per lectura del fitxer de nom nomFitxer.
-    fitxer =open("curses/" 
-    #Heu de pintar les tres primeres posicions 
-    print("Or ",)
-    print("Plata ",)
-    print("Bronze ",)
+  #nomFitxer conté el nom de l'arxiu que està en el directori curses 
+  #TODO heu de posar a la variable fitxer l'obertura per lectura del fitxer de nom nomFitxer.
+  fitxer =open("curses/"+nomFitxer)
+  contingut = fitxer.readline()
+  contingut2 = fitxer.readline()
+  contingut3 = fitxer.readline()
+  #Heu de pintar les tres primeres posicions 
+  print("Or ",contingut)
+  print("Plata ",contingut2)
+  print("Bronze ",contingut3)
